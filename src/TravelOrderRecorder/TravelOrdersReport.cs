0348633831e73da73a092e0daef9c101fdaec1c9
Boris Turk <boris.turk@xlab.si>
2016-12-11 22:15:10 +0100

    TravelOrder report.

---
+++ src/TravelOrderRecorder/TravelOrdersReport.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using AutomationEngine;
+
+namespace TravelOrderRecorder
+{
+    public class TravelOrdersReport : IPlugin
+    {
+        public string Id => "travel_orders_report";
+
+        public void Execute(params string[] arguments)
+        {
+            List<string> dates = (
+                from travelOrder in TravelOrdersCollection.Instance.TravelOrders
+                where travelOrder.IsTravelOrder
+                select travelOrder.Date.ToString("d.M."))
+                .ToList();
+
+            string result = string.Join(", ", dates);
+
+            Clipboard.SetText(result);
+        }
+    }
+}
