139336db8462a2ca53306b17436b08f967f7db33
boris <boris@LAPTOP-NQDPB0CP>
2020-08-22 17:28:28 +0200

    Implementing new automation engine.

--- new_core/src/BTurk.Automation.Standard/RepositorySearchHandler.cs
+++ new_core/src/BTurk.Automation.Standard/RepositorySearchHandler.cs
@@ -4,7 +4,7 @@ using BTurk.Automation.Core.SearchEngine;
 
 namespace BTurk.Automation.Standard
 {
-    public class CommitSearchHandler : ISearchHandler
+    public class RepositorySearchHandler : ISearchHandler
     {
         public SearchResultsCollection Handle(SearchParameters parameters)
         {
